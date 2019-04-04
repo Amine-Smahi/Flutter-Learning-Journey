327eaba0fcfbe0b6825fe447b7a17b3f526c7b9a
Amine Smahi <mohammed.amine.smahi@gmail.com>
2019-04-04 21:13:34 +0200

    Add project files

---
+++ login/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+import 'home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final routes = <String, WidgetBuilder>{
+    LoginPage.tag: (context) => LoginPage(),
+    HomePage.tag: (context) => HomePage(),
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'loginapp',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+        fontFamily: 'Nunito',
+      ),
+      home: LoginPage(),
+      routes: routes,
+    );
+  }
+}
